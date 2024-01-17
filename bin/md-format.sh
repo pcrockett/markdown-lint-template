d6f5704f1bc69c8d3f9c7efefe14425d1021c6bf
Philip Crockett <contact@philcrockett.com>
2024-01-17 11:45:15 -0600

    fix file selection in md-format.sh and pre-commit

--- bin/md-format.sh
+++ bin/md-format.sh
@@ -10,19 +10,30 @@ set -Eeuo pipefail
 # environment variables:
 #
 # * `MARKDOWN_FORMAT_DIFF_FILTER`: defaults to "AM" -- "Added" and "Modified". during
-#   pre-commit, you probably only want to set this to "A", but during editing, "AM"
+#   pre-commit, you probably only want to set this to "*" (none), but during editing, "AM"
 #
 
+VERBOSE="${VERBOSE:-false}"
+
 format_file() {
+    test "${VERBOSE}" = "false" || echo "formatting ${1}..."
     remark --output "${1}" --frail "${1}"
 }
 
-main() {
+get_files() {
     local diff_filter="${MARKDOWN_FORMAT_DIFF_FILTER:-AM}"
+    # get unstaged files that match our search criteria
+    git diff --name-only --diff-filter "${diff_filter}"
+
+    # get all staged files
+    git diff --name-only --staged
+}
+
+main() {
     local error_files=()
     while read -r file; do
         format_file "${file}" || error_files+=("${file}")
-    done < <(git diff --name-only --diff-filter "${diff_filter}" | grep --perl-regexp '\.md$')
+    done < <(get_files | sort --stable | uniq | grep --perl-regexp '\.md$')
     if [ "${#error_files[@]}" -gt 0 ]; then
         echo
         echo "Files with warnings / errors:"
